142a14544732192133f86e9d0e071631de5afd1f
StephenDK <stephendklein08@gmail.com>
2023-05-21 22:49:37 -0700

    Event Loop Ticks

--- loop.js
+++ loop.js
@@ -9,14 +9,17 @@ myFile.runContents();
 
 function shouldContinue() {
     // Check 1: Any pending setTimeout, setInterval, setImmediate?
-    // Check 2: Any pending OS tasks? (Like server listenint to port)
+    // Check 2: Any pending OS tasks? (Like server listening to port)
     // Check 3: Any pending long running operations? (Like fs module)
     return pendingTimers.length || pendingOperations.length || pendingOperations.length
 }
 
 // Entire body executes in one "tick"
-while(shouldContinue()) {
+while(shouldContinue()) {   // Event loop simulation
 
+    // 1) Node looks in pendingTimers and checks if any functions are ready to be called
+
+    // 2) Node looks at pendingOSTasks and pendingOperations and calls relevant callbacks
 }
 
 // Exit back to terminal
